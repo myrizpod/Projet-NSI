f101e4c577f9a4d08d23995772978a9df641aa04
Jojau12 <157808588+Jojau12@users.noreply.github.com>
2024-02-23 14:44:50 +0100

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -4,6 +4,7 @@ class MenuEngine:
     #Everything related to the menu
     def __init__(self):
         self.screensize=[256,128]
+        self.in_shop=False
 
     def menu_draw(self):
         pyxel.camera(0,0)
@@ -22,30 +23,53 @@ class MenuEngine:
         #Title
         text_border("Ski Adventure",self.screensize[0]/2-26,10,1,11)
         
-        #Menu buttons
-        #start button
-        if self.screensize[0]/2-10 <= pyxel.mouse_x <= self.screensize[0]/2+10 and self.screensize[1]/2-4 <= pyxel.mouse_y <= self.screensize[1]/2+3:
-            text_border("START",self.screensize[0]/2-9,self.screensize[1]/2-1,1,11)
-        else:
-            text_border("START",self.screensize[0]/2-9,self.screensize[1]/2-2,1,3)
-        #shop button
-        if self.screensize[0]-179 <= pyxel.mouse_x <= self.screensize[0]-164 and self.screensize[1]-31 <= pyxel.mouse_y <= self.screensize[1]-24:
-            text_border("Shop",77,self.screensize[1]-29,1,11)
-        else:
-            text_border("Shop",77,self.screensize[1]-30,1,3)
-        #settings button
-        if self.screensize[0]-107 <= pyxel.mouse_x <= self.screensize[0]-75 and self.screensize[1]-31 <= pyxel.mouse_y <= self.screensize[1]-24:
-            text_border("Settings",self.screensize[0]-106,self.screensize[1]-29,1,11)
-        else:
-            text_border("Settings",self.screensize[0]-106,self.screensize[1]-30,1,3)
+        #Menu design
+        if self.in_shop==False:
+            
+            #start button
+            if self.screensize[0]/2-10 <= pyxel.mouse_x <= self.screensize[0]/2+10 and self.screensize[1]/2-4 <= pyxel.mouse_y <= self.screensize[1]/2+3:
+                text_border("START",self.screensize[0]/2-9,self.screensize[1]/2-1,1,11)
+            else:
+                text_border("START",self.screensize[0]/2-9,self.screensize[1]/2-2,1,3)
+            #shop button
+            if self.screensize[0]-179 <= pyxel.mouse_x <= self.screensize[0]-164 and self.screensize[1]-31 <= pyxel.mouse_y <= self.screensize[1]-24:
+                text_border("Shop",77,self.screensize[1]-29,1,11)
+                if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT): self.in_shop=True
+            else:
+                text_border("Shop",77,self.screensize[1]-30,1,3)
+            #settings button
+            if self.screensize[0]-107 <= pyxel.mouse_x <= self.screensize[0]-75 and self.screensize[1]-31 <= pyxel.mouse_y <= self.screensize[1]-24:
+                text_border("Settings",self.screensize[0]-106,self.screensize[1]-29,1,11)
+            else:
+                text_border("Settings",self.screensize[0]-106,self.screensize[1]-30,1,3)
 
 
+        #detection of selected interface
+        elif self.in_shop==True: self.shop_interface()
+        
+    #Shop interface
+    def shop_interface(self):
+        #clicked shop button
+        text_border("Shop",77,self.screensize[1]-29,1,11)
+        #detection of the mouse to leave the shop interface
+        if self.screensize[0]-179 <= pyxel.mouse_x <= self.screensize[0]-164 and self.screensize[1]-31 <= pyxel.mouse_y <= self.screensize[1]-24 and pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            self.in_shop=False
+        #boxes of selection
+        for x in range(81,166,14):
+            for y in range(33,49,15):
+                pyxel.rectb(x,y,12,13,1)
+        for x in range(81,166,14):
+            pyxel.rectb(x,65,12,13,1)
+        for x in range(81,166,14):
+            pyxel.rectb(x,82,12,13,1)
 
     def menu_update(self):
         if pyxel.btnp(pyxel.KEY_SPACE) or (self.screensize[0]/2-10 <= pyxel.mouse_x <= self.screensize[0]/2+10 and self.screensize[1]/2-4 <= pyxel.mouse_y <= self.screensize[1]/2+3 and pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT)):
             return True
         else:
             return False
+
+
 
 def text_border(text,x,y,borderColor,color):
     """draws text at position [x,y] of color color surrounded by a border of color borderColor. <-- Understandeable sentence
